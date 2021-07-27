e402df9368f5e413d3bc364405d617b08bd4f9a0
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-07-27 16:10:05 +0100

    Update implementStack.py

--- Data_Structures/Stacks/implementStack.py
+++ Data_Structures/Stacks/implementStack.py
@@ -7,5 +7,36 @@ class Stack():
     def __init__(self):
         self.items = []
 
-    
+    # Check if stack is empty
+    def isEmpty(self):
+        return self.items == []
 
+    # Add element to stack
+    def push(self, item):
+        self.items.insert(0, item)
+
+    # Remove top item
+    def pop(self):
+        self.items.pop(0)
+
+    # Check top item
+    def peek(self):
+        return self.items[0]
+
+    # Size of stack
+    def size(self):
+        return len(self.items)
+
+    def display(self):
+        print(self.items)
+
+
+myStack = Stack()
+myStack.push("hello")
+myStack.push(5)
+myStack.push(True)
+myStack.push(6.9)
+myStack.push("World")
+myStack.display()
+myStack.pop()
+myStack.display()
